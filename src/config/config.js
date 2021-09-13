056faa91fe7fa86644241dd1a2d4acd052597bc2
Muhammad Lawal Adamu <lawaldd@gmail.com>
2021-09-13 14:14:42 +0100

    Removed host for sequelize production config

--- src/config/config.js
+++ src/config/config.js
@@ -18,7 +18,6 @@ module.exports = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host: process.env.DB_SERVER,
     dialect: 'mysql',
   },
   production: {
