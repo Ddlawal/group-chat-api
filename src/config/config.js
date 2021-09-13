4de309091513419dbd3c3dbff760fda72929f1a3
Muhammad Lawal Adamu <lawaldd@gmail.com>
2021-09-13 15:29:46 +0100

    Added host to sequelize production config

--- src/config/config.js
+++ src/config/config.js
@@ -18,6 +18,7 @@ module.exports = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    host: process.env.DB_SERVER,
     dialect: 'mysql',
   },
   production: {
